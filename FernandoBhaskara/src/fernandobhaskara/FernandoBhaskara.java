fe996402a10d35d084d69347309ddc3c7476017f
Fernando-Miranda0 <131157945+Fernando-Miranda0@users.noreply.github.com>
2023-04-18 07:51:35 -0300

    Add files via upload

---
+++ FernandoBhaskara/src/fernandobhaskara/FernandoBhaskara.java
@@ -0,0 +1,20 @@
+
+package fernandobhaskara;
+
+public class FernandoBhaskara {
+
+    public static void main(String[] args) {
+        bhaskara2  b1 = new bhaskara2();
+      
+      b1.setA(2);
+      b1.setB(5);
+      b1.setC(-9);
+      b1.ValorDelta();
+      b1.Valorx1();
+      b1.Valorx2(); 
+      b1.Status();   
+        }
+    }
+    
+    
+
